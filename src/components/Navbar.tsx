33fb15269ef5e3fcdae879995a99c726949d18b6
mohalw731 <mohalw731@gmail.com>
2024-06-20 14:25:51 +0200

    added a menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import logo from "../assets/logo.svg";
 import hero from "../assets/hero.svg";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <nav>
@@ -12,14 +16,25 @@ function Navbar() {
         </ul>
         <img src={logo} alt="mekkis-logo" className="logo" />
         <ul>
-          <li>Updrag</li>
+          <li>Uppdarg</li>
           <li>Kontakt</li>
         </ul>
-
-        <div className="menu">
-          <span><HamburgerMenuIcon className="nav-icon"/></span>
+        
+        <div className="menu" onClick={() => setIsOpen(prev => !prev)}>
+          <span>
+            <HamburgerMenuIcon className="nav-icon" />
+          </span>
         </div>
       </nav>
+      <div className={`menu-list ${isOpen ? 'menu-open' : ''}`}>
+        <ul>
+          <li>Hem</li>
+          <li>Om oss</li>
+          <li>Uppdrag</li>
+          <li>Vår expertis</li>
+          <li>Kontakt</li>
+        </ul>
+      </div>
       <img src={hero} alt="" className="nav-gradient" />
     </>
   );
